5cc70fb6172a797591ed8023abf73e48cbae9b49
Stan dud <standudu7@gmail.com>
2021-05-28 09:21:24 +0200

    add module

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name="response",
+    version="0.1",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=["docutils>=0.3"],
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt", "*.rst"],
+        # And include any *.msg files found in the "hello" package, too:
+        "hello": ["*.msg"],
+    },
+    # metadata to display on PyPI,
+    # could also include long_description, download_url, etc.
+)
